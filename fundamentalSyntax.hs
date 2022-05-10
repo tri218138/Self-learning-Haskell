f024a7e66b5729ae74d89816b66a429cafde3391
tri218138 <80521604+tri218138@users.noreply.github.com>
2022-05-10 23:04:15 +0700

    Update fundamentalSyntax.hs

--- fundamentalSyntax.hs
+++ fundamentalSyntax.hs
@@ -1,2 +1,20 @@
+-- get k start elements
+head k ns
+
+-- get list without k start elements
+drop k ns
+
+-- get list without last element
+init ns
+
+-- get last element
+-- solution1
+ns !! (length ns - 1)
+-- solution2
+last x = head (reverse x) x
+
+-- create cycle list with k-first element(s)
+cycleList = take k (cycle([1..n]))
+
 -- remove element at index i of list ns
 removeIndex i ns = take i ns ++ drop (1 + i) ns
